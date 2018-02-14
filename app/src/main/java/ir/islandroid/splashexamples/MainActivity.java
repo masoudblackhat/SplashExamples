f3a2f49eb6730242adc5c2dc3356138f0801b792
hamid_hajilo <hamid.t711@gmail.com>
2018-02-14 11:58:53 +0330

    Initial commit hamid

---
+++ app/src/main/java/ir/islandroid/splashexamples/MainActivity.java
@@ -0,0 +1,20 @@
+package ir.islandroid.splashexamples;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        },2000);
+    }
+}
